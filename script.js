09959947618ced2c7563905364a7f7411ad52d2c
JFreyra <efreyra@stuy.edu>
2017-02-14 12:39:33 -0500

    forgot to commit changes at home

--- script.js
+++ script.js
@@ -7,17 +7,19 @@ var button = document.getElementById("clear");
 
 var fillSlate = function(){
     console.log("doin stuff");
+    var mouseX = event.clientX;
+    var mouseY = event.clientY;
     slateCon.beginPath();
     slateCon.fillStyle = "#000000";
-    slateCon.fillRect(50,50,100,200);
+    slateCon.fillRect(mouseX-20,mouseY-20,20,20);
 };
 
 var clearSlate = function(){
     console.log("doin other stuff");
     slateCon.beginPath();
     slateCon.fillStyle = "#ffffff";
-    slateCon.fillRect(50,50,100,200);
+    slateCon.fillRect(0,0,1000,300);
 };
 
 button.addEventListener('click',clearSlate);
-slate.addEventListener('mouseover',fillSlate);
+slate.addEventListener('mousedown',fillSlate);
